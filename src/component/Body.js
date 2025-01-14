93affd0c4ddffa5d37916ec8ee5907f79a8c9404
nikkita07 <nikkitasingh64@gmail.com>
2025-01-14 09:16:07 +0530

    fetching API and shimmer UI

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,14 +1,26 @@
 import RestaurantCard from "./RestaurantCard.js";
-import resList from "../../utils/mockData.js";
-import { useState } from "react";
+// import resList from "../../utils/mockData.js";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer.js";
 
 
 const Body =()=>{
 
-  const [listOfRestaurants,setListOfRestaurant] = useState(resList);
-  
+  const [listOfRestaurants,setListOfRestaurant] = useState([]);
+   useEffect(()=>{
+    fetchData(); //fetch data from API
+   },[])
 
+ const fetchData =async () =>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants )
+ };
 
+  if(listOfRestaurants.length===0){
+   return <Shimmer/>
+  }
     return(
         <div className="body">
              <div className="filter">
